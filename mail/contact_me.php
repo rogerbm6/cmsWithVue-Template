ace7a1f1e9a3ac08561c71c33dd68310b0bc5867
rogerbm6 <roggerbm6@gmail.com>
2020-08-20 22:31:07 +0100

    prueba mail
    probando headers para el mail

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -18,7 +18,21 @@ $to = 'julymiraclean@gmail.com'; // Add your email address inbetween the '' repl
 $email_subject = "Está siendo contactada por:  $name";
 $email_body = "Usted ha recibido un nuevo mensaje de su página web.\n\n"."Estos son los detalles:\n\nNombre: $name\n\nCorreo electronico: $email_address\n\nMensaje:\n$message";
 $headers = "De: noResponder@julyMira.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
-$headers .= "Reply-To: $email_address";	
+
+//prueba
+//para el envío en formato HTML 
+$headers = "MIME-Version: 1.0\r\n"; 
+$headers .= "Content-type: text/html; charset=iso-8859-1\r\n"; 
+
+//dirección del remitente 
+$headers .= "De: Limpiezas JulyMira <julymiraclean@gmail.com>\r\n"; 
+
+//dirección de respuesta, si queremos que sea distinta que la del remitente 
+$headers .= "Reply-To: $email_address \r\n"; 
+
+//ruta del mensaje desde origen a destino 
+$headers .= "Return-path: julymiraclean@gmail.com\r\n"; 
+
 mail($to,$email_subject,$email_body,$headers);
 return true;			
 ?>
