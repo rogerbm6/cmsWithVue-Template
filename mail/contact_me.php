a98a08d1d0a693965f1845d85caf0f8c05c79f74
rogerbm6 <roggerbm6@gmail.com>
2022-06-03 12:10:37 +0100

    cambio de logo y colores
    cambiamos el logo, los colores y un poco el contenido en el apartado de servicios, tambien se actualizó el email

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -14,10 +14,10 @@ $email_address = $_POST['email'];
 $message = $_POST['message'];
 	
 // Create the email and send the message
-$to = 'julymiraclean@gmail.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
+$to = 'info@julymira.es'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
 $email_subject = "Está siendo contactada por:  $name";
 $email_body = "Usted ha recibido un nuevo mensaje de su página web.\n\n"."Estos son los detalles:\n\nNombre: $name\n\nCorreo electronico: $email_address\n\nMensaje:\n$message";
-$headers = "De: noResponder@julyMira.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
+$headers = "De: noResponder@julyMira.es\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
 $headers .= "Reply-To: $email_address";	
 mail($to,$email_subject,$email_body,$headers);
 return true;			
